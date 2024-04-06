607407e057c0f7a199ccf405407065312f2f8eda
jpenagos32 <jpenagosdev@gmail.com>
2024-04-06 02:05:39 -0500

    Agregar la maquetacion del proyecto

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 // import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
   // const [count, setCount] = useState(0)
@@ -9,15 +10,50 @@ function App() {
     <>
       <div className="drum-machine" id="drum-machine">
         <div className="drum-pad">
-          <div className="drum-pad" id="Heater-1">Q</div>
-          <div className="drum-pad" id="Heater-2">W</div>
-          <div className="drum-pad" id="Heater-3">E</div>
-          <div className="drum-pad" id="Heater-4">A</div>
-          <div className="drum-pad" id="Clap">S</div>
-          <div className="drum-pad" id="Open-HH">D</div>
-          <div className="drum-pad" id="Kick-n-hat">Z</div>
-          <div className="drum-pad" id="Kick">X</div>
-          <div className="drum-pad" id="Closed-HH">C</div>
+          <div className="drum-pad" id="Heater-1">
+            <audio src="https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3" className="clip" id="Q"></audio>
+            Q
+          </div>
+
+          <div className="drum-pad" id="Heater-2">
+            <audio src="https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3" className="clip" id="W"></audio>
+            W
+          </div>
+
+          <div className="drum-pad" id="Heater-3">
+            <audio src="https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3" className="clip" id="E"></audio>
+            E
+          </div>
+
+          <div className="drum-pad" id="Heater-4">
+            <audio src="https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3" className="clip" id="A"></audio>
+            A
+          </div>
+
+          <div className="drum-pad" id="Clap">
+            <audio src="https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3" className="clip" id="S"></audio>
+            S
+          </div>
+
+          <div className="drum-pad" id="Open-HH">
+            <audio src="https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3" className="clip" id="D"></audio>
+            D
+          </div>
+
+          <div className="drum-pad" id="Kick-n-hat">
+            <audio src="https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3" className="clip" id="Z"></audio>
+            Z
+          </div>
+
+          <div className="drum-pad" id="Kick">
+            <audio src="https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3" className="clip" id="X"></audio>
+            X
+          </div>
+
+          <div className="drum-pad" id="Closed-HH">
+            <audio src="https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3" className="clip" id="C"></audio>
+            C
+          </div>
         </div>
         <p id="display">aqui va el display</p>
       </div>
